b2ac71d0f03cb10b83bec54dbb62de0d6a40b914
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-23 12:43:35 -0700

    fixing errors with maven

--- src/main/java/io/github/bellxalli/centroidFinder/EuclideanColorDistance.java
+++ src/main/java/io/github/bellxalli/centroidFinder/EuclideanColorDistance.java
@@ -1,4 +1,3 @@
-
 package io.github.bellxalli.centroidFinder;
 
 public class EuclideanColorDistance implements ColorDistanceFinder {
--- src/test/java/io/github/bellxalli/centroidFinder/EuclideanColorDistanceTest.java
+++ src/test/java/io/github/bellxalli/centroidFinder/EuclideanColorDistanceTest.java
@@ -1,3 +1,5 @@
+package io.github.bellxalli.centroidFinder;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
