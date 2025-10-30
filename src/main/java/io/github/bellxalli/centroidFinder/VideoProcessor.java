0c4067e8b7235dbfa42b2fe1545efb7cd22e781e
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-29 21:38:18 -0700

    AI used, got the first few lines written for getting a new video from input vid, have to throw exceptions incase things go wrong

--- src/main/java/io/github/bellxalli/centroidFinder/VideoProcessor.java
+++ src/main/java/io/github/bellxalli/centroidFinder/VideoProcessor.java
@@ -13,5 +13,28 @@ import java.io.IOException;
 import java.util.List;
 
 public class VideoProcessor {
-    
+    private final DistanceImageBinarizer binarizer;
+    private final DfsBinaryGroupFinder groupFinder;
+
+    public VideoProcessor(DistanceImageBinarizer binarizer, DfsBinaryGroupFinder groupFinder) 
+    {
+        this.binarizer = binarizer;
+        this.groupFinder = groupFinder;
+    }
+
+
+    //get frame from video processor that will then be manipulated 
+    //using preexisting classes and their methods 
+    //  imageBinarizer and DfsgroupFinder to help
+    // convert to binarized img
+    // then turn frames into new video
+    public void processVideo(File input, File output)
+    {
+        //getting frame and creating new path for manipulated frames to be written out to and by
+        FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(input));
+        SequenceEncoder encoder = new SequenceEncoder(NIOUtils.writableChannel(output));
+
+
+    }
+
 }
