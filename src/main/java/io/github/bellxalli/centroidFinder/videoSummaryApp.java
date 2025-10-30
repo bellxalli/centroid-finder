c129ab4d3d9d02003a7ee5a3600aa259c7e16414
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-29 22:38:35 -0700

    made main method but now working on getting it to take inputs from the command line

--- src/main/java/io/github/bellxalli/centroidFinder/videoSummaryApp.java
+++ src/main/java/io/github/bellxalli/centroidFinder/videoSummaryApp.java
@@ -1,22 +1,31 @@
 package io.github.bellxalli.centroidFinder;
 
 //imports
-import org.jcodec.api.FrameGrab;
-import org.jcodec.api.JCodecException;
-import org.jcodec.common.io.NIOUtils;
-import org.jcodec.common.model.Picture;
-import org.jcodec.scale.AWTUtil;
-
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 public class videoSummaryApp {
     public static void main(String[] args)
     {
+        //creating files to grab video and send output data to        
         File input = new File("ballsMoving.mp4");
-        File output = new File("output_bw.mp4");
+        File output = new File("output.csv");
 
-        //use video processor 
+        //pick targetColor
+        int targetColor = 1;//from commandline?;
+        int threshold = 1;//also commandline?;
+
+        //use euclidean color distance finder, binarizer, dsfgroupfinder
+        ColorDistanceFinder distance = new EuclideanColorDistance();
+        DistanceImageBinarizer binarizer = new DistanceImageBinarizer(distance, targetColor, threshold);
+        DfsBinaryGroupFinder groupFinder = new DfsBinaryGroupFinder();
+
+        //use video processor
+        VideoProcessor processor = new VideoProcessor(binarizer, groupFinder);
+
+        //process video and get csv
+        processor.processVideo(input, output);
+
+        //tells me what's happened
+        System.out.println("Video processed successfully! Results saved to: " + output.getAbsolutePath());
     }
 }
